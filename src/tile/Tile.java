3544b9944ea598caea890ef5d9b965fd19bb650d
alfredredbird <105014217+Alfredredbird@users.noreply.github.com>
2023-05-10 15:42:40 -0700

    Update Tile.java

--- src/tile/Tile.java
+++ src/tile/Tile.java
@@ -1,7 +1,7 @@
 package tile;
 
 import java.awt.image.BufferedImage;
-
+//this defines tile properties. 
 public class Tile {
     public BufferedImage image;
     public boolean collision = false;
