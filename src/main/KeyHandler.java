8ba865223095e4ea1305190093b85cd993860f82
alfredredbird <105014217+Alfredredbird@users.noreply.github.com>
2023-05-22 11:55:55 -0700

    added mod healing. sprint need to be fixed tho

--- src/main/KeyHandler.java
+++ src/main/KeyHandler.java
@@ -12,7 +12,7 @@ import java.security.PublicKey;
 public class KeyHandler implements KeyListener {
 
     GamePanel gp;
-    public boolean upPressed, downPressed, leftPressed, rightPressed, enterPressed, sprint;
+    public boolean upPressed, downPressed, leftPressed, rightPressed, enterPressed;
     //debug
     boolean DrawTimeMenu = false;
 
@@ -245,27 +245,9 @@ public class KeyHandler implements KeyListener {
 
     public void playState(int code){
 
-        if(code == KeyEvent.VK_CONTROL){
-
-            if(sprint == true){
-                sprint = false;
-            }
-            if(sprint == false){
-                sprint = true;
-            }
-        }
-        if (code == KeyEvent.VK_W){
-            if(sprint == true){
+        if (code == KeyEvent.VK_W ){
                 upPressed = true;
                 gp.player.rest = false;
-                gp.player.speed = 6;
-            }
-            if(sprint == false) {
-                upPressed = true;
-                gp.player.rest = false;
-                gp.player.speed = 2;
-            }
-
         }
         if (code == KeyEvent.VK_A){
             leftPressed = true;
@@ -303,6 +285,7 @@ public class KeyHandler implements KeyListener {
             }
         }
     }
+
     public void pauseState(int code){
         if (code == KeyEvent.VK_ESCAPE){
             gp.gameState =gp.playState;
